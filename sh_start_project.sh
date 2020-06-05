f667a20d1f5ae88e26a7c57951199fb933642014
Davi <davicervo@gmail.com>
2020-06-05 16:05:57 -0300

    alterando shell

--- sh_start_project.sh
+++ sh_start_project.sh
@@ -1,16 +1,24 @@
+#!/bin/bash
 #inciando o projeto
-#git checkout develop
-#git pull
-#composer install
-#cp .env.localhost .env
-#composer dumpautoload
-#php artisan key:generate
+echo "### Inciando o projeto ###"
 
-#host do windows
+echo "Verificando branch"
+git checkout develop
+
+echo "Baixando dados do projeto"
+git pull
+
+echo "Instalando dependências e inciando o projeto"
+composer install
+cp .env.localhost .env
+composer dumpautoload
+php artisan key:generate
+
+##host do windows
 
 #vhosts
-host_url=api_form_cci
-project_dir=$(pwd)
+host_url=laravel_shell
+project_dir="${PWD/#$HOME/C:/Users/$USERNAME}"
 apache_dir="D:\xampp\apache\conf\extra\httpd-vhosts.conf"
 # shellcheck disable=SC2089
 new_vhost="\n\n
@@ -26,6 +34,11 @@ new_vhost="\n\n
 echo -e $new_vhost >>$apache_dir
 
 #reiniciando o xampp
+echo "Reiniciando o apache..."
 cd /d/xampp
 start apache_stop.bat
 start apache_start.bat
+
+echo "O projeto esta rodando em: http://$host_url.localhost"
+echo "### Fim da instalação ###"
+
